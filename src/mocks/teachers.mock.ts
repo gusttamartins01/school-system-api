276c6720233b7fd0b1ee004b91d9aebbc70b1219
gustavo martins <luizgustams@gmail.com>
2026-07-22 19:52:51 -0300

    chore: add mock teacher data

---
+++ src/mocks/teachers.mock.ts
@@ -0,0 +1,11 @@
+import type { Teacher } from '../types.ts';
+
+export const teachers: Teacher[] = [
+	{
+		id: 1,
+		name: 'irineu Souza',
+		email: 'prof.irineu@email.com',
+		cod_rp: 'a1b2c3d4e5',
+		turma: 'fs-f7',
+	},
+];
