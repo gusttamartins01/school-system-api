ace85256cdad2021cf4666d3cad44bd91096d429
gustavo martins <luizgustams@gmail.com>
2026-07-22 19:52:51 -0300

    feat: add student service

---
+++ src/services/student.service.ts
@@ -0,0 +1,57 @@
+import { NotFoundError } from '../errors/index.ts';
+import { students } from '../mocks/students.mock.ts';
+import type { CreateStudent, Student, UpdateStudent } from '../types.ts';
+
+export function findAllStudents(): Student[] {
+	return students;
+}
+
+export function findStudentsById(id: number): Student {
+	const student = students.find((s) => s.id === id);
+
+	if (!student) throw new NotFoundError('Estudante não encontrado!');
+
+	return student;
+}
+
+export function insertStudents({
+	name,
+	email,
+	matricula,
+}: CreateStudent): Student {
+	const student: Student = {
+		id: students[students.length - 1].id + 1,
+		name: name,
+		email: email,
+		matricula: matricula,
+		turma: 'fs-57',
+	};
+
+	students.push(student);
+
+	return student;
+}
+
+export function modifyStudents(
+	id: number,
+	{ name, email, matricula, turma }: UpdateStudent,
+): Student {
+	const student = students.find((s) => s.id === id);
+
+	if (!student) throw new NotFoundError('Estudante não encontrado!');
+
+	if (name) student.name = name;
+	if (email) student.email = email;
+	if (matricula) student.matricula = matricula;
+	if (turma) student.turma = turma;
+
+	return student;
+}
+
+export function deleteStudents(id: number): void {
+	const index = students.findIndex((s) => s.id === id);
+
+	if (index === -1) throw new Error('Estudante não encontrado!');
+
+	students.splice(index, 1);
+}
