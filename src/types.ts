c9120c0cc6aa43383d168873bdb0b476761adf4f
gustavo martins <luizgustams@gmail.com>
2026-07-22 19:52:50 -0300

    feat: add shared type definitions

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type Student = {
+	id: number;
+	name: string;
+	email: string;
+	matricula: string;
+	turma: string;
+};
+
+export type CreateStudent = Omit<Student, 'id' | 'turma'>;
+export type UpdateStudent = Partial<Omit<Student, 'id'>>;
+
+export type Teacher = {
+	id: number;
+	name: string;
+	email: string;
+	cod_rp: string;
+	turma: string;
+};
+
+export type CreateTeacher = Omit<Teacher, 'id' | 'turma'>;
+export type UpdateTeacher = Partial<Omit<Teacher, 'id'>>;
