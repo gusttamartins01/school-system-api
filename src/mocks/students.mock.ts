4d0b464de88b1ea4dd7fae36fabf0d0ddf437cc9
gustavo martins <luizgustams@gmail.com>
2026-07-22 19:52:51 -0300

    chore: add mock student data

---
+++ src/mocks/students.mock.ts
@@ -0,0 +1,11 @@
+import type { Student } from '../types.ts';
+
+export const students: Student[] = [
+	{
+		id: 1,
+		name: 'gustavo martins',
+		email: 'gustavo.silva@email.com',
+		matricula: '1a2b3c4d5e',
+		turma: 'fs-57',
+	},
+];
