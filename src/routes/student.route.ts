10055a7bbb08ad6ab90fcc7e27da9a93be654ca5
gustavo martins <luizgustams@gmail.com>
2026-07-23 19:32:56 -0300

    feat: add validation for creating and updating customers to the routes

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import * as StudentController from '../controllers/student.controller.ts';
+import validate from '../middlewares/validate.ts'
+import  { 
+    createStudentSchema,
+    updateStudentSchema
+} from '../schemas/student.schema.ts'
+
 
 const router = Router();
 
 router.get('/', StudentController.getAllStudent);
-router.get('/:id', StudentController.getStudentById);
-router.post('/', StudentController.createStudent);
+router.get('/:id', validate(createStudentSchema), StudentController.getStudentById);
+router.post('/', validate(updateStudentSchema), StudentController.createStudent);
 router.put('/:id', StudentController.updateStudent);
-router.delete('/:id', StudentController.deletreStudent);
+router.delete('/:id', StudentController.deleteStudent);
 
 export default router;
