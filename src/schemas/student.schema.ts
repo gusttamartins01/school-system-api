ee971965f8e34cb41804d232d9431e883779b65c
gustavo martins <luizgustams@gmail.com>
2026-07-23 18:52:04 -0300

    feat: add types Create, Update Schemas

--- src/schemas/student.schema.ts
+++ src/schemas/student.schema.ts
@@ -6,5 +6,14 @@ export const createStudentSchema = z.object({
     matricula: z.string().min(1).max(10),
 });
 
+export const updateStudentSchema = z.object({
+    name: z.string().min(1).optional(),
+    email: z.email().optional(),
+    matricula: z.string().min(1).max(10).optional(),
+    turma: z.string().min(1).optional(),
+});
+
+export type CreateCustomerInput = z.infer<typeof createStudentSchema>;
+export type UpdateCustomerInput = z.infer<typeof updateStudentSchema>;
 
 
